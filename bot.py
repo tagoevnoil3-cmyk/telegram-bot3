7ab76d2a3884aad9baea2a28420b36cdde7e87cd
tagoevnoil3-cmyk <tagoevnoil3@gmail.com>
2026-01-08 15:21:50 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import telebot
 import os
-TOKEN = os.getenn"8057069793:AAGUfWr8OVPZKOCPX34JvMUk9eaBTf5jXHg"
+print("TOKEN = 8057069793:AAGUfWr8OVPZKOCPX34JvMUk9eaBTf5jXHg",os.getenv("BOT.TOKEN"))
 if not TOKEN:
 	raise ValueError("BOT_TOKEN" is not set")                                      
 	bot = telebot.Telebot(TOKEN)
@@ -8,4 +8,5 @@ if not TOKEN:
 	def start(message):
 		bot.send_messssage(message.chat.id."Бот раотает") 
 		bot.infinity_polling()
+
 
